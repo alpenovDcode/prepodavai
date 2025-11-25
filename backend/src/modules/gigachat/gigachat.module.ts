baa6a105d1932b80fe8a1748a757b0a4f65d3cb3
alpenovDcode <alpenovevgeniy05@gmail.com>
2025-11-25 13:06:33 +0300

    worksheet fix8

--- backend/src/modules/gigachat/gigachat.module.ts
+++ backend/src/modules/gigachat/gigachat.module.ts
@@ -6,11 +6,12 @@ import { GigachatGenerationsService } from './gigachat-generations.service';
 import { GenerationsModule } from '../generations/generations.module';
 import { SubscriptionsModule } from '../subscriptions/subscriptions.module';
 import { FilesModule } from '../files/files.module';
+import { HtmlExportService } from '../../common/services/html-export.service';
 
 @Module({
   imports: [ConfigModule, forwardRef(() => GenerationsModule), SubscriptionsModule, FilesModule],
   controllers: [GigachatController],
-  providers: [GigachatService, GigachatGenerationsService],
+  providers: [GigachatService, GigachatGenerationsService, HtmlExportService],
   exports: [GigachatService, GigachatGenerationsService],
 })
 export class GigachatModule {}
